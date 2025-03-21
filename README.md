d466b02d12dcdd448234e909d9d48f46032fba64
Daniel Herzog <wikiti.doghound@gmail.com>
2025-03-21 17:04:00 +0000

    Add contributors list

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ A simple web app to play sounds during a Metal Gear Solid by CMON play session. 
 
 https://wikiti.github.io/metal-gear-solid-soundboard
 
+The app was mainly built using GitHub Copilot since I didn't want to over engineer this simple project.
+
 ## Development
 
 This is a regular react app, so just check `package.json` for commands, and play around with it!
+
+## Contributors
+
+This web app was developed by Daniel @wikiti, with the help of Eric @Zypzu and other BGG community members.
--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ function App() {
         />
       </div>
 
-      <div className="disclaimer">
+      <div className="disclaimer warning">
         <p>
           <strong>Disclaimer:</strong> This soundboard does not store or host any audio files on this server. 
           All sounds are streamed directly from external sources (archives, etc). 
@@ -100,6 +100,12 @@ function App() {
           operates as a reference tool only. All Metal Gear Solid audio content, 
           characters, and related media are property of Konami. 
           This is an unofficial fan project not affiliated with or endorsed by Konami.
+        </p>
+      </div>
+
+      <div className="disclaimer">
+        <p>
+          This web app was developed by <a target="_blank" href="https://github.com/wikiti" rel="noreferrer">Daniel @wikiti</a>, with the help of <a target="_blank" href="https://boardgamegeek.com/user/Zypzu" rel="noreferrer">Eric @Zypzu</a> and other BGG community members.
         </p>
       </div>
     </div>
